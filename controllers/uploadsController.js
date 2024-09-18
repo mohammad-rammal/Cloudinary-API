8221485883533afc3cc95b6faa46aa1574d1a72b
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-09-18 20:45:37 +0300

    Cloudinary Controller

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
     // console.log(req.files);
 
     if (!req.files) {
@@ -25,6 +27,32 @@ const uploadProductImage = async (req, res) => {
     await productImage.mv(imagePath);
 
     return res.status(StatusCodes.OK).json({image: {src: `/uploads/${productImage.name}`}});
+};
+
+const uploadProductImage = async (req, res) => {
+    try {
+
+        const now = new Date();
+        const timestamp = now.toISOString().replace(/[:.]/g, '-');
+        const originalFilename = path.basename(req.files.image.name, path.extname(req.files.image.name));
+        const customFilename = `${originalFilename}-${timestamp}`;
+
+
+        
+        const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+            public_id: customFilename, 
+            use_filename: true,
+            folder: 'file-upload',
+        });
+
+        fs.unlinkSync(req.files.image.tempFilePath);
+
+        
+        return res.status(StatusCodes.OK).json({image: {src: result.secure_url}});
+    } catch (error) {
+        console.error('Error uploading image to Cloudinary:', error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error: 'Error uploading image'});
+    }
 };
 
 module.exports = {
